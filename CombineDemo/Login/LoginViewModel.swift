901272190d9a8ef27eea07bca59934c40df6a64e
mcichecki <mcichecki@me.com>
2020-12-20 11:35:25 +0100

    Add CredentialsValidator

--- CombineDemo/Login/LoginViewModel.swift
+++ CombineDemo/Login/LoginViewModel.swift
@@ -18,13 +18,47 @@ final class LoginViewModel {
         .map { $0.count > 2 && $1.count > 2 }
         .eraseToAnyPublisher()
     
+    private let credentialsValidator: CredentialsValidatorProtocol
+    
+    init(credentialsValidator: CredentialsValidatorProtocol = CredentialsValidator()) {
+        self.credentialsValidator = credentialsValidator
+    }
+    
     func validateCredentials() {
         isLoading = true
         
+        credentialsValidator.validateCredentials(login: login, password: password) { [weak self] result in
+            self?.isLoading = false
+            switch result {
+            case .success:
+                self?.validationResult.send(())
+            case let .failure(error):
+                self?.validationResult.send(completion: .failure(error))
+            }
+        }
+    }
+}
+
+// MARK: - CredentialsValidatorProtocol
+
+protocol CredentialsValidatorProtocol {
+    func validateCredentials(
+        login: String,
+        password: String,
+        completion: @escaping (Result<(), Error>) -> Void)
+}
+
+/// This class acts as an example of asynchronous credentials validation
+/// It's for demo purpose only. In the real world it would make an actual request or use other authentication method
+final class CredentialsValidator: CredentialsValidatorProtocol {
+    func validateCredentials(
+        login: String,
+        password: String,
+        completion: @escaping (Result<(), Error>) -> Void) {
         let time: DispatchTime = .now() + .milliseconds(Int.random(in: 200 ... 1_000))
         DispatchQueue.main.asyncAfter(deadline: time) {
-            self.isLoading = false
-            self.validationResult.send(())
+            // hardcoded success
+            completion(.success(()))
         }
     }
 }
